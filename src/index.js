a67b1c55d0495fb8cec87dc8e224c4789cec29fc
NguyenTienHieu-GitHub <you@example.com>
2024-07-24 13:10:44 +0700

    Install Morgan and Handlebars

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+// Templates engine
+app.engine('hbs', hbs.engine({ extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+   
+// HTTP logger
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
