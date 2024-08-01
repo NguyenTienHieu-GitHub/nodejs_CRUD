b85ff2ed7dd19252bb7624073b48426a1640ae47
NguyenTienHieu-GitHub <you@example.com>
2024-08-01 13:40:25 +0700

    [CRUD] DELETE

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -45,6 +45,13 @@ class CourseController {
       .then(() => res.redirect('/me/stored/courses'))
       .catch(next);
   }
+
+  //[DELETE] /courses/:id
+  delete(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
+  }
 }
 
 // GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD
