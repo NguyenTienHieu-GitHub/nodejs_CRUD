f4bfa0b7a240d15990a2c6c28a4946b0dfec21f8
NguyenTienHieu-GitHub <you@example.com>
2024-08-05 19:13:44 +0700

    SelectAll with checkBox

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -64,6 +64,29 @@ class CourseController {
       .then(() => res.redirect("back"))
       .catch(next);
   }
+  //[POST] /courses/handle-form-actions
+  handleFormActions(req, res, next) {
+    switch (req.body.action) {
+      case "delete":
+        Course.delete({ _id: { $in: req.body.courseIds } })
+          .then(() => res.redirect("back"))
+          .catch(next);
+        break;
+        S;
+      case "delete-force":
+        Course.deleteMany({ _id: { $in: req.body.courseIds } })
+          .then(() => res.redirect("back"))
+          .catch(next);
+        break;
+      case "restore":
+        Course.restore({ _id: { $in: req.body.courseIds } })
+          .then(() => res.redirect("back"))
+          .catch(next);
+        break;
+      default:
+        res.json({ message: "Action is invalid!" });
+    }
+  }
 }
 
 // GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD
